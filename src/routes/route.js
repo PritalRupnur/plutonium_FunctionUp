30153814273f666d8b844776a50b22ceeb356f6e
PritalRupnur <premalrupnur2001@gmail.com>
2022-08-10 16:44:32 +0530

    post api-1

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
+ 
     console.log(studentName)
     res.send(studentName)
 })
