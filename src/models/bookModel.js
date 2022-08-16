60e1f286ab28740d080f5aff4127abadc0e94ea9
PritalRupnur <premalrupnur2001@gmail.com>
2022-08-16 19:07:31 +0530

    bookschema

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -8,6 +8,15 @@ const bookSchema = new mongoose.Schema( {
     category: String,
     
     year: Number,
+    tags:[string],//it implies array of strings
+    isPublished:Boolean, 
+    date: {Date,
+            default:Date.now},
+    prices: {
+            indianPrice:string,
+            europePrice:String
+    },
+    sales:{type:Number, default:10} 
    
 }, { timestamps: true });
 
