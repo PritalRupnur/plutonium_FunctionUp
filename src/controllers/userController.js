0b98fb5cc1f6e8d0c5f46bb63dedd5770916a82c
PritalRupnur <premalrupnur2001@gmail.com>
2022-08-28 20:33:35 +0530

    jwt1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,7 @@ const createUser = async function (abcd, xyz) {
   //the second parameter is always the response
   let data = abcd.body;
   let savedData = await userModel.create(data);
-  console.log(abcd.newAtribute);
+ // console.log(abcd.newAtribute);
   xyz.send({ msg: savedData });
 };
 
@@ -88,11 +88,31 @@ const updateUser = async function (req, res) {
   }
 
   let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, {$set:{userData}});
   res.send({ status: updatedUser, data: updatedUser });
 };
 
+
+
+const deletedUser = async function (req, res) {
+  // Do the same steps here:
+  // Check if the token is present
+  // Check if the token present is a valid token
+  // Return a different error message in both these cases
+
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  //Return an error if no user with the given id exists in the db
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+ 
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId },{$set:{isDeleted:true}},{new:true});
+  res.send({ status: true, data: updatedUser });
+}
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deletedUser = deletedUser;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-
+const authentication = require("../authentication/auth")
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/users", authentication.authToken,userController.createUser  )
 
-router.post("/login", userController.loginUser)
+router.post("/login",authentication.authToken, userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/users/:userId",authentication.authToken, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/users/:userId", authentication.authToken,userController.updateUser)
+router.put("/users1/:userId", authentication.authToken,userController.deletedUser)
 
 module.exports = router;
