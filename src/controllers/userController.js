64201318c2faba2913b81a2859b56171757f5ea4
PritalRupnur <premalrupnur2001@gmail.com>
2022-08-30 23:21:01 +0530

    try catch method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,53 @@ const userModel = require("../models/userModel");
 
 
 
+
+const createBook = async function (req, res) {
+  try {
+      let data = req.body
+      console.log(data)
+      if ( Object.keys(data).length != 0) {
+          let savedData = await BookModel.create(data)
+          res.status(201).send({ msg: savedData })
+      }
+      else res.status(400).send({ msg: "BAD REQUEST"})
+  }
+  catch (err) {
+      console.log("This is the error :", err.message)
+      res.status(500).send({ msg: "Error", error: err.message })
+  }
+}
+
+// TRY CATCH SUMMARY:
+// if you get an error in try block, it will not execute the next lines of code inside try
+// instead it will jump into catch block and execute the code there
+// code in catch block is normallly not executed
+//rather catch block is only executed if there is error in try block
+// the error( along with message++) gets sent to catch block incase there is an error
+
+
+
+
+// Specific HTTP codes(only impt ones)
+// 2xx- Success
+// 4xx- something gone wrong..and problem is on user side(client side)
+// 5xx- server side problems
+
+// "BAD REQUEST" ...400..say if username password dont match etc..or anything generic( any problem in input on user side or any other unhandled problem)
+// "RESOURCE NOT FOUND"...404 //404 page not found...eg. find ("asaijndianud89")...let book =bookModel.findOne({_id:"asaijndianud89"})   if (book){..} else res.status(404).send({})
+// "AUTHENTICATION MISSING"...401..login is required...if(token){...} else { res.status(401)}
+// "NOT AUTHENTICATED OR FORBIDDEN"..403 // if ( token.userId === userId) {...} else {res.status(403).send({}) }
+// -- try catch ....// "SERVER ERROR"...500
+
+// -- ALL GOOD... //status(200)- OK
+// --- "ALL GOOD and A NEW RESOURCE WAS SUCCEFULLY CREATED" ...status(201)..e.g a new user registers herself successfully
+
+
+
+
+
+
+
 //______________________________________________________________________________________//
 
 const createUser = async function (abcd, xyz) {
@@ -17,16 +64,17 @@ const createUser = async function (abcd, xyz) {
 //______________________________________________________________________________________//
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
+  try{let userName = req.body.emailId;
   let password = req.body.password;
 
   let user = await userModel.findOne({ emailId: userName, password: password });
+  
   if (!user)
     return res.send({
       status: false,
       msg: "username or the password is not corerct",
     });
-
+  
   
   let token = jwt.sign(
     {
@@ -37,28 +85,40 @@ const loginUser = async function (req, res) {
     "functionup-thorium"
   );
   res.setHeader("x-auth-token", token);
-  res.send({ status: true, data: token });
+  res.status(201).send({ status: true, data: token });
+  }
+  catch (err) {
+    //console.log("This is the error :", err.message)
+    res.status(501).send({ msg: "Error", error: err.message })
+}
 };
 
 //______________________________________________________________________________________//
 
 const getUserData = async function (req, res) {
   
+  try{
+    let userId = req.params.userId;
+    console.log(req.params.userId)
+   // let userID = ObjectId(userId)
+    let userDetails = await userModel.findById({_id:userId});
+    if (!userDetails)
+      return res.send({ status: false, msg: "No such user exists" });
   
- 
-  let userId = req.params.userId;
-  console.log(req.params.userId)
- // let userID = ObjectId(userId)
-  let userDetails = await userModel.findById({_id:userId});
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
+      res.status(201).send({ status: true, data: userDetails });
 
-  res.send({ status: true, data: userDetails });
+  }
+  catch (err) {
+    //console.log("This is the error :", err.message)
+    res.status(501).send({ msg: "Error", error: err.message })
+}
+ 
+ 
 };
 //______________________________________________________________________________________//
 
 const updateUser = async function (req, res) {
-
+try{
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
   //Return an error if no user with the given id exists in the db
@@ -68,13 +128,19 @@ const updateUser = async function (req, res) {
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
-  res.send({ status: updatedUser, data: updatedUser });
+  res.status(201).send({ status: updatedUser, data: updatedUser });
+}
+catch (err) {
+  //console.log("This is the error :", err.message)
+  res.status(501).send({ msg: "Error", error: err.message })
+}
 };
 
 
 //______________________________________________________________________________________//
 
 const postMessage = async function (req, res) {
+  try{
     let message = req.body.message
     
 
@@ -88,10 +154,16 @@ const postMessage = async function (req, res) {
     let updatedUser = await userModel.findOneAndUpdate({_id: user._id},{posts: updatedPosts}, {new: true})
 
   
-    return res.send({status: true, data: updatedUser})
+    return res.status(201).send({status: true, data: updatedUser})
+  }
+  catch (err) {
+    //console.log("This is the error :", err.message)
+    res.status(501).send({ msg: "Error", error: err.message })
 }
-
+}
+//______________________________________________________________________________//
 const deletedUser = async function (req, res) {
+  try{
  
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
@@ -103,6 +175,11 @@ const deletedUser = async function (req, res) {
  
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId },{$set:{isDeleted:true}},{new:true});
   res.send({ status: true, data: updatedUser });
+}
+catch (err) {
+  //console.log("This is the error :", err.message)
+  res.status(401).send({ msg: "Error", error: err.message })
+}
 }
 
 module.exports.createUser = createUser;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,20 +5,25 @@ const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
     //console.log(req.headers)
-    let token = req.headers["x-Auth-token"];
+    try{ let token = req.headers["x-Auth-token"];
    
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-   req.token = token
+    if (!token) token = req.headers["x-auth-token"];
+  
+    //If no token is present in the request header return error
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+     req.token = token}
+     catch (err) {
+        //console.log("This is the error :", err.message)
+        res.status(401).send({ msg: "Error", error: err.message })
+      }
+   
     next()
 }
 
 
 const authorise = function(req, res, next) {
   
-  
+  try{
     let decodedToken = jwt.verify(req.token, 'functionup-thorium')
 
     if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
@@ -31,6 +36,11 @@ const authorise = function(req, res, next) {
     //userId comparision to check if the logged-in user is requesting for their own data
     if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
 
+  }
+  catch (err) {
+    //console.log("This is the error :", err.message)
+    res.status(401).send({ msg: "Error", error: err.message })
+  }
     next()
 }
 module.exports.authenticate= authenticate;
